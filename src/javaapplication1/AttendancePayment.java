6e865ae575415fb4b96ef4c85de3a784474ad676
michael-obroin <michael.obroin@students.ignatius.org>
2017-01-07 13:43:27 -0600

    Added suggestions to clean up code from netbeans

--- src/javaapplication1/AttendancePayment.java
+++ src/javaapplication1/AttendancePayment.java
@@ -97,6 +97,7 @@ public class AttendancePayment extends javax.swing.JFrame
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     
     public static void main(String args[]) throws IOException 
@@ -123,15 +124,11 @@ public class AttendancePayment extends javax.swing.JFrame
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AttendancePayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AttendancePayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AttendancePayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(AttendancePayment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        
         //</editor-fold>
 
         /* Create and display the form */
